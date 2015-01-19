49d5857d7956548fe60f4adced09c6e8810188e3
Alejandro Herce <alexherce94@gmail.com>
2015-01-19 14:11:14 -0600

    Suma de matriz

--- recursividadSumaArray2D/recursividadSumaArray2D/main.cpp
+++ recursividadSumaArray2D/recursividadSumaArray2D/main.cpp
@@ -9,26 +9,26 @@
 #include <iostream>
 using namespace std;
 
-int sumaArray2D(int ** arreglo, int x, int y);
+int sumaMatriz(int ** arreglo, int x, int y);
 
 int main(int argc, const char * argv[]) {
     
     int a[3][3] = {3,2,1,4,6,4,3,2,6};
     cout << "La suma del arreglo en 2D es: ";
-    cout << sumaArray2D(a, 3, 3) << endl;
+    cout << sumaMatriz(a, 3, 3) << endl;
     
     
     return 0;
 }
 
-int sumaArray2D(int ** arreglo, int x, int y)
+int sumaMatriz(int ** arreglo, int x, int y)
 {
     if(x == 0 && y == 0)
     {
         return arreglo[0][0];
     } else if (y == 0){
-        return arreglo[x][y] + sumaArray2D(arreglo, x - 1, y - 1);
+        return arreglo[x][y] + sumaMatriz(arreglo, x - 1, y - 1);
     } else {
-        return arreglo[x][y] + sumaArray2D(arreglo, x, y - 1);
+        return arreglo[x][y] + sumaMatriz(arreglo, x, y - 1);
     }
 }
