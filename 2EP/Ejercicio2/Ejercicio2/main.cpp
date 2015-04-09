55b144e5da58f4668ae3fc55ee0715eb6ed24616
Alejandro Herce <alexherce94@gmail.com>
2015-04-09 13:36:18 -0500

    Ejercicio 2 listo

---
+++ 2EP/Ejercicio2/Ejercicio2/main.cpp
@@ -0,0 +1,65 @@
+//
+//  main.cpp
+//  Ejercicio2
+//
+//  Created by Alejandro Herce on 09/04/15.
+//  Copyright (c) 2015 Alejandro Herce. All rights reserved.
+//
+
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace vcn;
+using namespace std;
+
+LinkedList<int> listL;
+LinkedList<int> listR;
+void listaRepetida();
+
+int main(int argc, const char * argv[]) {
+    
+    listL.insertBack(5);
+    listL.insertBack(2);
+    listL.insertBack(7);
+    listL.insertBack(2);
+    listL.insertBack(5);
+    listL.insertBack(5);
+    listL.insertBack(1);
+    
+    cout << listL << endl;
+    
+    listaRepetida();
+    
+    cout << endl << endl;
+    
+    cout << listR << endl;
+    
+    
+    return 0;
+}
+
+
+void listaRepetida()
+{
+    int temp1;
+    int temp2;
+    int contVar = 0;
+    for (int i = 0; i < listL.size(); i++)
+    {
+        contVar = 0;
+        temp1 = listL.at(i)->getInfo();
+        for(int j = 0; j < listL.size(); j++)
+        {
+            temp2 = listL.at(j)->getInfo();
+            if (temp1 == temp2)
+            {
+                if (contVar == 1)
+                {
+                    listR.insertBack(temp1);
+                    contVar++;
+                }
+                contVar++;
+            }
+        }
+    }
+}
