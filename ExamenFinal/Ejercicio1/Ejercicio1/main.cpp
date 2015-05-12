6efc7f032684096ec4a9057e68a0d309b7b767e1
Alejandro Herce <alexherce94@gmail.com>
2015-05-12 14:24:15 -0500

    Ejercicio 1

---
+++ ExamenFinal/Ejercicio1/Ejercicio1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ejercicio1
+//
+//  Created by Alejandro Herce on 12/05/15.
+//  Copyright (c) 2015 Alejandro Herce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
